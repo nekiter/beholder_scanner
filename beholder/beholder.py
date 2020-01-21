8a54fd782d4b9e199bdaca147fb08f72845c75a3
Lucas Vale <araujo.lucasvale@gmail.com>
2020-01-20 21:28:06 -0300

    [New] Adding function to attach on a process

--- beholder/beholder.py
+++ beholder/beholder.py
@@ -5,21 +5,62 @@ from logger import Logger
 
 class Beholder:
 
-    scanner = None
-
+    scanners = None
     logger = None
 
     def __init__(self):
         self.logger = Logger(logger_name="beholder", logger_level="DEBUG")
-
-        self.scanner = Scanner()
-
+        self.scanners = []
         self.logger.info("Beholder initialized!")
 
     def attach_to_process(self, pid: int = -1) -> bool:
-        self.scanner.set_pid(pid)
+        if pid <= -1:
+            self.logger.error("Invalid PID!")
+            return False
+
+        for scanner in self.scanners:
+            if scanner.get_pid() == pid:
+                self.logger.warning("This process is already attached!")
+                return False
+
+        scanner = Scanner(pid=pid)
+        self.scanners.append(scanner)
         return True
+
+    def scan_value(self, value=None, value_type: str = "int") -> dict:
+        if len(self.scanners) == 0:
+            self.logger.warning("Can't scan without attach to a process.")
+            return {}
+
+        if value is None:
+            self.logger.error("Can't scan None value.")
+            return {}
+
+        for scanner in self.scanners:
+            scanner.search_value(value=value, value_type=value_type)
+
+        return {}
 
 
 if __name__ == "__main__":
     teste = Beholder()
+    while True:
+        print("="*100)
+        print("Choose one option:")
+        print("1- Attach to a process")
+        print("2- Scan a value")
+        print("="*100)
+        option = int(input())
+
+        if option < 0 or option > 2:
+            print("Invalid option!")
+            break
+
+        if option == 1:
+            print("\nWrite the PID of the process")
+            pid = int(input())
+            teste.attach_to_process(pid=pid)
+        elif option == 2:
+            print("\nWrite the value to search")
+            value_to_scan = int(input())
+            teste.scan_value(value=value_to_scan, value_type="int")
--- beholder/scanner.py
+++ beholder/scanner.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python3
+from logger import Logger
+
+
 class Scanner:
 
     pid = -1
-
+    logger = None
     memory_map = None
 
-    def __init__(self, int: pid = -1):
+    def __init__(self, pid: int = -1):
         self.set_pid(pid)
+        self.logger = Logger(logger_name=f"scanner({pid})",
+                             logger_level="DEBUG")
         self.set_memory_map()
         print("Scanner initialized!")
 
-    def set_pid(self, int: pid = -1) -> bool:
+    def set_pid(self, pid: int = -1) -> bool:
         if pid <= 0:
             raise Exception("I can't attach to a process with pid <= 0")
-        self.píd = pid
+        self.pid = pid
         return True
 
     def get_pid(self) -> int:
-        return pid
+        return self.pid
 
     def set_memory_map(self):
         self.memory_map = open(f"/proc/{self.pid}/maps", 'r')
 
-    def search_value(self, value, str: type = "int"):
+    def get_memory_map(self) -> str:
+        string_to_return = "addr\tperm\toffset\tdevice\tinode\tpath_name"
+        for line in self.memory_map:
+            splited_line = line.split(' ')
+            addr = splited_line[0]
+            perm = splited_line[1]
+            offset = splited_line[2]
+            device = splited_line[3]
+            inode = splited_line[4]
+            pathname = splited_line[-1][:-1]
+
+            string_to_return += f"\n{addr}\t{perm}\t{offset}\t{device}\t{inode}\t{pathname}"
+        return string_to_return
+
+    def search_value(self, value, value_type: str = "int"):
         raise Exception("seach_value no implemented")
 
 
